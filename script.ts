9e22cb5d59c77219095b910c750f20cad0a75e34
unknown <juliesg@tut.by>
2021-08-20 14:59:56 +0300

    Added bundled file .js

--- script.ts
+++ script.ts
@@ -14,11 +14,10 @@ interface IData {
 }
 
 //function for getting data from an endpoint
-async function getUsers(url: string): Promise<any> {
+async function getUsers(url: string) {
     try {
         const response = await fetch(url);
         const users = await response.json();
-        console.log(users.value)
         return users.value;
     } catch(error) {
         console.log(error);
@@ -32,23 +31,23 @@ async function mergeData(url1: string, url2: string): Promise<void> {
     const dataPart1: IData[] = await getUsers(url1);
     const dataPart2: IData[] = await getUsers(url2);
 
-    console.log("hello2")
-
     const getMap = (data: any, id: string) => new Map(data.map(({[id]:_id, ...res}) => [_id, {...res}]));
 
     const setID = [...new Set(dataPart1.map(({ID}) => ID)
-        .concat(dataPart2.map(({ProductID}) => ProductID)))];
+        .concat(dataPart2.map(({ProductID}) => ProductID))
+    )];
 
     const dataPart1Mapped: Map<any, any> = getMap(dataPart1, 'ID');
     const dataPart2Mapped: Map<any, any> = getMap(dataPart2, 'ProductID');
 
-    const result = setID.map(id => ({
+    //merging both data's
+    const resultData = setID.map(id => ({
         ...(dataPart1Mapped.get(id) || {}),
         ...(dataPart2Mapped.get(id) || {})
     })
     );
 
-    console.log(result);
+    console.log(resultData);
 }
 
 
